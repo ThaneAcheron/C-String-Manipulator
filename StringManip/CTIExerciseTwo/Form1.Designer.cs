5c81557ef77cddba732b93d2b0b028f959172925
ThaneAcheron <craig.turner944@gmail.com>
2018-04-23 10:28:08 +0200

    Final

---
+++ StringManip/CTIExerciseTwo/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CTIExerciseTwo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChar = new System.Windows.Forms.Button();
+            this.btnNumber = new System.Windows.Forms.Button();
+            this.btnRemoveLast = new System.Windows.Forms.Button();
+            this.brnUpperCase = new System.Windows.Forms.Button();
+            this.btnLowerCase = new System.Windows.Forms.Button();
+            this.btnNormalCase = new System.Windows.Forms.Button();
+            this.lblPassWord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChar
+            // 
+            this.btnChar.Location = new System.Drawing.Point(12, 104);
+            this.btnChar.Name = "btnChar";
+            this.btnChar.Size = new System.Drawing.Size(81, 34);
+            this.btnChar.TabIndex = 0;
+            this.btnChar.Text = "Add Char";
+            this.btnChar.UseVisualStyleBackColor = true;
+            this.btnChar.Click += new System.EventHandler(this.btnChar_Click);
+            // 
+            // btnNumber
+            // 
+            this.btnNumber.Location = new System.Drawing.Point(99, 104);
+            this.btnNumber.Name = "btnNumber";
+            this.btnNumber.Size = new System.Drawing.Size(81, 34);
+            this.btnNumber.TabIndex = 1;
+            this.btnNumber.Text = "Add Number";
+            this.btnNumber.UseVisualStyleBackColor = true;
+            this.btnNumber.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btnRemoveLast
+            // 
+            this.btnRemoveLast.Location = new System.Drawing.Point(186, 104);
+            this.btnRemoveLast.Name = "btnRemoveLast";
+            this.btnRemoveLast.Size = new System.Drawing.Size(81, 34);
+            this.btnRemoveLast.TabIndex = 2;
+            this.btnRemoveLast.Text = "Remove Last";
+            this.btnRemoveLast.UseVisualStyleBackColor = true;
+            this.btnRemoveLast.Click += new System.EventHandler(this.btnRemoveLast_Click);
+            // 
+            // brnUpperCase
+            // 
+            this.brnUpperCase.Location = new System.Drawing.Point(12, 144);
+            this.brnUpperCase.Name = "brnUpperCase";
+            this.brnUpperCase.Size = new System.Drawing.Size(81, 34);
+            this.brnUpperCase.TabIndex = 3;
+            this.brnUpperCase.Text = "Upper Case";
+            this.brnUpperCase.UseVisualStyleBackColor = true;
+            this.brnUpperCase.Click += new System.EventHandler(this.brnUpperCase_Click);
+            // 
+            // btnLowerCase
+            // 
+            this.btnLowerCase.Location = new System.Drawing.Point(99, 144);
+            this.btnLowerCase.Name = "btnLowerCase";
+            this.btnLowerCase.Size = new System.Drawing.Size(81, 34);
+            this.btnLowerCase.TabIndex = 4;
+            this.btnLowerCase.Text = "Lower Case";
+            this.btnLowerCase.UseVisualStyleBackColor = true;
+            this.btnLowerCase.Click += new System.EventHandler(this.btnLowerCase_Click);
+            // 
+            // btnNormalCase
+            // 
+            this.btnNormalCase.Location = new System.Drawing.Point(186, 144);
+            this.btnNormalCase.Name = "btnNormalCase";
+            this.btnNormalCase.Size = new System.Drawing.Size(81, 34);
+            this.btnNormalCase.TabIndex = 5;
+            this.btnNormalCase.Text = "Normal Case";
+            this.btnNormalCase.UseVisualStyleBackColor = true;
+            this.btnNormalCase.Click += new System.EventHandler(this.btnNormalCase_Click);
+            // 
+            // lblPassWord
+            // 
+            this.lblPassWord.AutoSize = true;
+            this.lblPassWord.Location = new System.Drawing.Point(107, 51);
+            this.lblPassWord.Name = "lblPassWord";
+            this.lblPassWord.Size = new System.Drawing.Size(56, 13);
+            this.lblPassWord.TabIndex = 6;
+            this.lblPassWord.Text = "PassWord";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 219);
+            this.Controls.Add(this.lblPassWord);
+            this.Controls.Add(this.btnNormalCase);
+            this.Controls.Add(this.btnLowerCase);
+            this.Controls.Add(this.brnUpperCase);
+            this.Controls.Add(this.btnRemoveLast);
+            this.Controls.Add(this.btnNumber);
+            this.Controls.Add(this.btnChar);
+            this.Name = "Form1";
+            this.Text = "Password Creator ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChar;
+        private System.Windows.Forms.Button btnNumber;
+        private System.Windows.Forms.Button btnRemoveLast;
+        private System.Windows.Forms.Button brnUpperCase;
+        private System.Windows.Forms.Button btnLowerCase;
+        private System.Windows.Forms.Button btnNormalCase;
+        private System.Windows.Forms.Label lblPassWord;
+    }
+}
+
---
+++ StringManip/CTIExerciseTwo/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+namespace CTIExerciseTwo
+{
+    public partial class Form1 : Form
+    {
+        ArrayList characters = new ArrayList();
+        ArrayList integers = new ArrayList();
+        string originalValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblPassWord.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChar_Click(object sender, EventArgs e)
+        {
+            int randomnumber;
+            int UpperRandomNumber; 
+
+            Random number = new Random();
+            randomnumber = number.Next(1, 26);
+            UpperRandomNumber = number.Next(1, 3);
+
+            if (randomnumber == 1)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "a";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "A";
+                }
+            }
+            else if (randomnumber == 2)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "b";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "B";
+                }
+            }
+            else if (randomnumber == 3)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "c";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "C";
+                }
+            }
+            else if (randomnumber == 4)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "d";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "D";
+                }
+            }
+            else if (randomnumber == 5)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "e";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "E";
+                }
+            }
+            else if (randomnumber == 6)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "f";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "F";
+                }
+            }
+            else if (randomnumber == 7)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "g";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "G";
+                }
+            }
+            else if (randomnumber == 8)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "h";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "H";
+                }
+            }
+            else if (randomnumber == 9)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "i";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "I";
+                }
+            }
+            else if (randomnumber == 10)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "j";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "J";
+                }
+            }
+            else if (randomnumber == 11)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "k";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "K";
+                }
+            }
+            else if (randomnumber == 12)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "l";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "L";
+                }
+            }
+            else if (randomnumber == 13)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "m";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "M";
+                }
+            }
+            else if (randomnumber == 14)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "n";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "N";
+                };
+            }
+            else if (randomnumber == 15)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "o";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "O";
+                }
+            }
+            else if (randomnumber == 16)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "p";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "P";
+                }
+            }
+            else if (randomnumber == 17)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "q";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "Q";
+                }
+            }
+            else if (randomnumber == 18)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "r";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "R";
+                }
+            }
+            else if (randomnumber == 19)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "s";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "S";
+                }
+            }
+            else if (randomnumber == 20)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "t";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "T";
+                }
+            }
+            else if (randomnumber == 21)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "u";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "U";
+                }
+            }
+            else if (randomnumber == 22)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "v";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "V";
+                }
+            }
+            else if (randomnumber == 23)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "w";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "W";
+                }
+            }
+            else if (randomnumber == 24)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "x";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "X";
+                }
+            }
+            else if (randomnumber == 25)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "y";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "Y";
+                }
+            }
+            else if (randomnumber == 26)
+            {
+                if (UpperRandomNumber == 1)
+                {
+                    lblPassWord.Text = lblPassWord.Text + "z";
+                }
+                else
+                {
+                    lblPassWord.Text = lblPassWord.Text + "Z";
+                }
+            }
+            
+        
+        }
+
+        private void btnNumber_Click(object sender, EventArgs e)
+        {
+            Random number = new Random();
+            lblPassWord.Text = lblPassWord.Text + number.Next(0, 9).ToString();
+        }
+
+        private void btnRemoveLast_Click(object sender, EventArgs e)
+        {
+            string labelvalue = lblPassWord.Text;
+            string newLabelValue; 
+
+            int lastletter = labelvalue.Count();
+            lastletter = lastletter - 1;
+
+            newLabelValue = labelvalue.Remove(lastletter);
+
+            lblPassWord.Text = newLabelValue;
+        }
+
+        private void brnUpperCase_Click(object sender, EventArgs e)
+        {
+            originalValue = lblPassWord.Text; 
+            lblPassWord.Text = lblPassWord.Text.ToUpper();
+        }
+
+        private void btnLowerCase_Click(object sender, EventArgs e)
+        {
+            originalValue = lblPassWord.Text; 
+            lblPassWord.Text = lblPassWord.Text.ToLower();
+        }
+
+        private void btnNormalCase_Click(object sender, EventArgs e)
+        {
+            lblPassWord.Text = originalValue;
+        }
+    }
+}
